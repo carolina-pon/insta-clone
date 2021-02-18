4d592d4825767e258476579a8d21174fc02d4366
nagata <mad.20th@gmail.com>
2021-02-18 13:19:52 +0900

    CRUD機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # localhost:3000を開いたとき、usersコントローラのnewアクションを呼び出す。
   root 'users#new'
 
-  resources :users, only: %i[new create] # %記法 => [:new, :create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sesssions#destroy'
+
+  resources :users, only: %i[new create] # %記法 => [:new, :create]
+  resources :posts
+  
 end
