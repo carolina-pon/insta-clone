b2edc937445691ab28826098d65ded00dd1939c8
lina <mad.20th@gmail.com>
2022-02-17 18:44:10 +0900

    modify user一覧を新しい順/降順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.page(params[:page])
+    # pageはkaminariのメソッド
+    @users = User.all.page(params[:page]).order(created_at: :desc)
   end
 
   def show
