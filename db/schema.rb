5cd183502d3baaf588085c4914e8f528a9dd2670
lina <mad.20th@gmail.com>
2022-10-03 18:40:11 +0900

    add 通知オンオフのフラグ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_233629) do
+ActiveRecord::Schema.define(version: 2022_10_03_093417) do
 
   create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "subject_type"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 2022_05_24_233629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.boolean "notification_on_comment", default: true
+    t.boolean "notification_on_like", default: true
+    t.boolean "notification_on_follow"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
