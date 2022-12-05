cabeb43c48c1a6afada33e51d8a8b46ea8f2137c
lina <mad.20th@gmail.com>
2022-12-05 13:56:42 +0900

    add memo

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,7 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     # ログインユーザーがあるユーザーにいいねされた時、通知をオンにしていればメールが届く
+    # UserMailerでmailerに定義したメソッドを呼び出す。withにはキーの値を渡せる。コントローラでいうparamsと同じ要領。
     UserMailer.with(user_from: current_user, user_to: @post.user, post: @post).like_post.deliver_later if current_user.like(@post) && @post.user.notification_on_like?
   end
 
