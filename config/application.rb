8405209bbbec7d86c43b9a291e88f7c309f3e232
lina <mad.20th@gmail.com>
2022-12-15 20:40:29 +0900

    fix rubocopによる修正

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,9 @@ module InstaClone
       g.assets false # CSS,JSを自動生成しない
       g.skip_routes true # ルーティングを自動生成しない
       g.test_framework :rspec,
-      view_specs: false,
-      helper_specs: false,
-      routing_specs: false
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
     end
   end
 end
